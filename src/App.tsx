d63103c5582b1bf072f170dec7e7e3314b11d2e6
Rami <138556019+rami242424@users.noreply.github.com>
2026-02-09 10:00:00 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 function App(){
+  const [count, setCount] = useState(0);
   return (
-    <h1>Plus Minus Reset</h1>
+    <>
+      <h1>Plus Minus Reset</h1>
+      <h2>{count}</h2>
+    </>
   );
 }
 
