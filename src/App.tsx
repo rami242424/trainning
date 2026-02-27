d61223dbf984eb1aa3b42187cb9db50e108cc21c
Rami <138556019+rami242424@users.noreply.github.com>
2026-02-27 10:00:00 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import { useState } from "react";
 
 function App(){
-  const [count, setCount] = useState(0);
-  const onIncrease = () => {
-    setCount((prev) => prev + 1);
-  }
-  const onDecresase = () => {
-    setCount((prev) => prev - 1);
-  }
-  const onReset = () => {
-    setCount(0);
-  }
+  
   return (
     <>
-      <h1>Plus Minus Reset</h1>
-      <h2>{count}</h2>
-      <button onClick={onIncrease} disabled={count >= 10}>Plus</button>
-      {count >= 10 && <span style={{ color: "red"}}>Max!</span>}
-      <button onClick={onDecresase} disabled={count <= 0}>Minus</button>
-      <button onClick={onReset}>Reset</button>
     </>
   );
 }
