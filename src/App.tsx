c85cfec1ec3d3be318fd4179c518df51ec20dfb6
Rami <138556019+rami242424@users.noreply.github.com>
2026-02-28 10:00:00 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
 function App(){
-  
+  const inputChange = (e) => {
+    console.log(e.target.value)
+  }
   return (
     <>
+      <input onChange={inputChange}/>
     </>
   );
 }
