c125f6c47893e269ed2e6683983ffd7c18c6a375
Rami <138556019+rami242424@users.noreply.github.com>
2026-03-02 10:00:00 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ function App(){
   const inputChange = (e) => {
     console.log(e.target.value)
   }
+  const addClick = () => {
+    //누르면 저장 > 리스트로 만들어지고
+    //인풋은 리셋되어야함
+  }
   return (
     <>
       <input onChange={inputChange}/>
-      <button>Add</button>
+      <button onClick={addClick}>Add</button>
     </>
   );
 }
