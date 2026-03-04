f3cdd219e3d2798a72ec8211139c11c7c43276e0
Rami <138556019+rami242424@users.noreply.github.com>
2026-03-04 10:00:00 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 
 function App(){
+  const [inputValue, setInputValue] = useState("");
+  //add시 저장될공간
+  const [items, setItems] = useState([]);
+
   const inputChange = (e) => {
-    console.log(e.target.value)
+    //console.log(inputValue, "inputvalue1")
+    setInputValue(e.target.value)
+    //console.log(inputValue, "inputvalue2")
   }
   const addClick = () => {
     //누르면 저장 > 리스트로 만들어지고
+    //setItems(...items, inputValue);
+    //더 안전한 방법
+    setItems((prev) => [...prev, inputValue]);
     //인풋은 리셋되어야함
+    setInputValue("");
   }
   return (
     <>
       <input onChange={inputChange}/>
       <button onClick={addClick}>Add</button>
+      {items.map((item) => <li>{item}</li>)}
     </>
   );
 }
