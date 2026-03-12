88923a7ef5a7499b7182feed0b1cd88251f289fb
Rami <138556019+rami242424@users.noreply.github.com>
2026-03-12 10:00:00 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,4 @@
-// import { useState } from "react";
-
 import { useState } from "react";
-
-// function App(){
-//   const [inputValue, setInputValue] = useState("");
-//   const [items, setItems] = useState<string[]>([]);
-//   const inputChange = (e) => {
-//     setInputValue(e.target.value)
-//   }
-//   const addClick = () => {
-//     setItems((prev) => [...prev, inputValue]);
-//     setInputValue("");
-//   }
-//   const deleteClick = (index) => {
-//     setItems(items.filter((item, i) => i !== index));
-//   }
-//   return (
-//     <>
-//       <input value={inputValue} onChange={inputChange}/>
-//       <button onClick={addClick}>Add</button>
-//       {items.map((item, index) => (
-//         <>
-//           <li key={index}>
-//             {item}
-//             <button onClick={() => deleteClick(index)}>✖️</button>
-//           </li>
-//         </>
-//       ))}
-//     </>
-//   );
-// }
-
-// export default App;
 
 function App(){
   const [inputValue, setInputValue] = useState("");
@@ -40,6 +7,7 @@ function App(){
     setInputValue(e.target.value);
   }
   const addBtnClick = () => {
+    if(inputValue.trim()) return;
     setItems((prev) => [...prev, inputValue]);
     setInputValue("");
   }
