ecd4ac0810ff2f0e146d0737c1c10fb94aaf5a33
Rami <138556019+rami242424@users.noreply.github.com>
2026-03-12 10:00:00 +0900

    commit

---
+++ src/prac.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function App(){
+    const [value, setValue] = useState("");
+    const [lists, setLists] = useState([]);
+    const onChange = (e) => {
+        setValue(e.target.value);
+    }
+    const onClick = () => {
+        setLists((prev) => [...prev, value]);
+        setValue("");
+    }
+    return(
+        <>
+            <input value={value} onChange={onChange}/>
+            <button onClick={onClick}>Add</button>
+            {lists.map((list, index) => <li key={index}>{list}<button>X</button></li>)}
+        </>
+    );
+}
+
+export default App;
