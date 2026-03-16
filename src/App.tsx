d26a8c31958433b3fef3e401dfcfdd35689e24c8
Rami <138556019+rami242424@users.noreply.github.com>
2026-03-16 21:05:04 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,4 @@
-// import { useState } from "react";
-
 import { useState } from "react";
-
-
-// interface ItemType {
-//   id: number;
-//   text: string;
-// }
-// function App(){
-//   const [inputValue, setInputValue] = useState("");
-//   const [items, setItems] = useState<ItemType[]>([]);
-//   const [edictingId, setEdictingId] = useState<number | null>(null);
-//   const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-//     setInputValue(e.target.value);
-//   }
-//   const addBtnClick = () => {
-//     if(!inputValue.trim()) return;
-//     // 수정
-//     if(edictingId !== null) {
-//       setItems((prev) => prev.map((item) => 
-//         edictingId === item.id ? {...item, text: inputValue} : item
-//       ))
-//     } else {
-//     // 추가
-//     setItems((prev) => [...prev, {id: Date.now(), text: inputValue}]);
-//     }
-//     setInputValue("");
-//   }
-//   const deleteBtn = (id:number) => {
-//     setItems(items.filter((item) => item.id !== id));
-//   }
-//   const editBtn = (item:ItemType) => {
-//     setEdictingId(item.id);
-//     setInputValue(item.text);
-//     console.log(edictingId, "에딕팅아이디")
-//   }
-//   return(
-//     <>
-//       <input value={inputValue} onChange={inputChange}/>
-//       <button onClick={addBtnClick}>{edictingId !== null ? "SAVE" : "ADD"}</button>
-//       {items.map((item) => (
-//         <li key={item.id}>
-//           {item.text}
-//           <button onClick={() => deleteBtn(item.id)}>✖️</button>
-//           <button onClick={() => editBtn(item)}>Edit</button>
-//         </li>
-//         ))}
-//     </>
-//   );
-// }
-
-// export default App;
 
 interface Itype {
   id: number;
@@ -79,9 +27,7 @@ function App(){
       // 
       setItems((prev) => [...prev, {id: Date.now(), text: inputValue}]);
     }
-    setInputValue("");
-
-    
+    setInputValue("");  
   }
   const deleteBtn = (id:number) => {
     setItems((prev) => prev.filter((item) => item.id !== id));
@@ -91,7 +37,6 @@ function App(){
     setEditingId(item.id)
     setInputValue(item.text)
     //add버튼이 save버튼으로 바뀌어야함
-    
   }
   return(
     <>
