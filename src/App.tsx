d949eeb7c3d85d4096222532d5bb9cc6c95c1c3b
Rami <138556019+rami242424@users.noreply.github.com>
2026-03-17 14:15:16 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,4 @@
-// import { useState } from "react";
-
 import { useState } from "react";
-
-// interface Itype {
-//   id: number;
-//   text: string;
-// }
-
-// function App(){
-//   const [inputValue, setInputValue] = useState("");
-//   const [items, setItems] = useState<Itype[]>([]);
-//   const [editingId, setEditingId] = useState<number | null>(null);
-//   const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-//     setInputValue(e.target.value)  
-//   }
-//   const addClick = () => {
-//     if(!inputValue.trim()) return;
-//     if(editingId !== null) {
-//       setItems((prev) => (
-//         //prev.map((item) => editingId === item.id ? {id: item.id, text: inputValue} : item)
-//         prev.map((item) => editingId === item.id ? {...item, text: inputValue} : item)
-//       ));
-//       setEditingId(null);
-
-//     } else {
-//       setItems((prev) => [...prev, {id: Date.now(), text: inputValue}]);
-//     }
-//     setInputValue("");  
-//   }
-//   const deleteBtn = (id:number) => {
-//     setItems((prev) => prev.filter((item) => item.id !== id));
-//   }
-//   const editBtn = (item:Itype) => {
-//     setEditingId(item.id)
-//     setInputValue(item.text)
-//   }
-//   return(
-//     <>
-//       <input onChange={inputChange} value={inputValue}/>
-//       <button onClick={addClick}>{editingId !== null ? "SAVE" : "ADD"}</button>
-//       {items.map((item) => (
-//         <li key={item.id}>
-//           {item.text}
-//           <button onClick = {() => deleteBtn(item.id)}>delete</button>
-//           <button onClick = {() => editBtn(item)}>edit</button>
-//         </li>
-//       ))}
-//     </>
-//   );
-// }
-
-// export default App;
 
 interface IType {
   id: number;
@@ -67,7 +15,7 @@ function App(){
   const addClick = () => {
     if(!inputValue.trim()) return;
     if(editingId !== null){
-      //수정중이다
+      //수정중
       setItems((prev) => prev.map((item) => editingId === item.id ? {...item, text: inputValue}: item));
     } else{
       // 수정x, 단순 추가o
@@ -87,8 +35,8 @@ function App(){
     <>
       <input value={inputValue} onChange={inputChange}/>
       <button onClick={addClick}>{editingId !== null ? "SAVE" : "ADD"}</button>
-      {items.map((item, id) => (
-        <li key={id}>
+      {items.map((item) => (
+        <li key={item.id}>
           {item.text}
           <button onClick={() => {deleteBtn(item.id)}}>delete</button>
           <button onClick={() => {editBtn(item)}}>edit</button>
