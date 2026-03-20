094139488c8f47ff5d108f40857bf1870cd13fb2
Rami <138556019+rami242424@users.noreply.github.com>
2026-03-20 16:12:46 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,4 @@
-//import { useState } from "react";
-
 import { useState } from "react";
-
-// interface IType {
-//   id: number;
-//   text: string;
-//   completed: boolean;
-// }
-
-// function App(){
-//   const [inputValue, setInputValue] = useState("");
-//   const [items, setItems] = useState<IType[]>([]);
-//   const [editingId, setEditingId] = useState<number|null>(null);
-//   const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-//     setInputValue(e.target.value);
-//   }
-//   const addClick = () => {
-//     if(!inputValue.trim()) return;
-//     if(editingId !== null){
-//       //수정중
-//       setItems((prev) => prev.map((item) => editingId === item.id ? {...item, text: inputValue}: item));
-//     } else{
-//       // 수정x, 단순 추가o
-//       setItems((prev) => [...prev, {id: Date.now(), text: inputValue, completed: false}]);
-//     }
-//     setInputValue("");
-//     setEditingId(null);
-//   }
-//   const deleteBtn = (id:number) => {
-//     setItems((prev) => prev.filter((item) => item.id !== id));
-//   }
-//   const editBtn = (item:IType) => {
-//     setEditingId(item.id);
-//     setInputValue(item.text);
-//   }
-//   const toggleCompleted = (id:number) => {
-//     setItems((prev) => 
-//       prev.map((item) => item.id === id ? {...item, completed: !item.completed} : item)
-//     );
-//   }
-//   return (
-//     <>
-//       <input value={inputValue} onChange={inputChange}/>
-//       <button onClick={addClick}>{editingId !== null ? "SAVE" : "ADD"}</button>
-//       {items.map((item) => (
-//         <li key={item.id}>
-//           <input 
-//             type="checkbox"
-//             checked={item.completed}
-//             onChange={() => toggleCompleted(item.id)}
-//           />
-//           <span
-//             style={{
-//               textDecoration: item.completed ? "line-through" : "none"
-//             }}>
-//             {item.text}
-//           </span>
-//           <button onClick={() => {deleteBtn(item.id)}}>delete</button>
-//           <button onClick={() => {editBtn(item)}}>edit</button>
-//         </li>
-//       ))}
-//     </>
-//   );
-// }
-
-// export default App;
 
 interface IType {
   id: number;
@@ -79,20 +13,12 @@ interface TodoInputProps {
   editingId: number | null;
 }
 
-interface TodoListProps {
-  items: IType[];
-  toggleCompleted: (id:number) => void;
-  deleteBtn: (id:number) => void;
-  editBtn: (item:IType) => void;
-}
-
 interface TodoItemProps {
   item: IType;
   toggleCompleted: (id: number) => void;
   deleteBtn: (id: number) => void;
   editBtn: (item: IType) => void;
 }
-
 
 function App(){
   const [inputValue, setInputValue] = useState("");
@@ -129,12 +55,16 @@ function App(){
         addBtn={addBtn}
         editingId= {editingId}
       />
-      <TodoList 
-        items={items}
-        toggleCompleted={toggleCompleted}
-        deleteBtn={deleteBtn}
-        editBtn={editBtn}
-      />
+      {items.map((item) => 
+        <li key={item.id}>
+          <TodoItem 
+            item={item}
+            toggleCompleted={toggleCompleted}
+            deleteBtn={deleteBtn}
+            editBtn={editBtn}
+          />
+        </li>
+      )}
     </>
   );
 }
@@ -148,23 +78,6 @@ function TodoInput({inputValue, inputChange, addBtn, editingId}: TodoInputProps)
       <button onClick={addBtn}>
         {editingId !== null ? "SAVE" : "ADD"}
       </button>
-    </>
-  );
-}
-
-function TodoList({items,toggleCompleted,deleteBtn,editBtn}:TodoListProps){
-  return(
-    <>
-      {items.map((item) => 
-        <li key={item.id}>
-          <TodoItem 
-            item={item}
-            toggleCompleted={toggleCompleted}
-            deleteBtn={deleteBtn}
-            editBtn={editBtn}
-          />
-        </li>
-      )}
     </>
   );
 }
