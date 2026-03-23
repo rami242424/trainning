fd0758201665d1440eed2a15ae895e5baac1286a
Rami <138556019+rami242424@users.noreply.github.com>
2026-03-23 10:00:00 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
+interface IType{
+  id: number;
+  text: string;
+  completed: boolean;
+}
 function App(){
+  const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState<IType[]>([]);
+  const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+  const addBtn = () => {
+    setItems((prev) => [...prev, {id:Date.now(), text:inputValue, completed:false}]);
+  }
   return(
     <>
-      <input />
-      <button>Add</button>
+      <input value="inputValue" onChange={inputChange}/>
+      <button onClick={addBtn}>Add</button>
+      {items.map((item) => 
+      <ul>
+        <li key={item.id}>{item.text}</li>
+        <button>delete</button>
+        <button>edit</button>
+      </ul>
+      )}
     </>
   );
 }
