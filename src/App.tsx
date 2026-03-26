3cae9c41f73788eeb80592b334132678372d3f34
Rami <138556019+rami242424@users.noreply.github.com>
2026-03-26 18:17:46 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,41 @@
 import { useState } from "react";
 
-interface IType{
-  id: number;
-  text: string;
-  completed: boolean;
+interface IType {
+  id:number;
+  text:string;
+  completed:boolean;
 }
 function App(){
   const [inputValue, setInputValue] = useState("");
   const [items, setItems] = useState<IType[]>([]);
-  const [editingId , setEditingId] = useState<number|null>(null);
-  const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-  }
+  const [editingId, setEditingId] = useState<number|null>(null);
   const addBtn = () => {
     if(!inputValue.trim()) return;
-    // 수정중
-    if(editingId !==null){
-      // setItems((prev) => prev.map((item) => editingId === item.id ? [...item, {text:inputValue}]: item));
-      setItems((item) => item.map((item) => item.id === editingId ? {...item, text:inputValue} : item));
+    if(editingId !== null){
+      setItems((item) => item.map((item) => editingId === item.id ? {...item, text:inputValue} : item));
     } else {
-      setItems((prev) => [...prev, {id:Date.now(), text:inputValue, completed:false}]);
+      setItems((item) => [...item, {id:Date.now(), text:inputValue, completed:false}]);
     }
     setInputValue("");
+    setEditingId(null);
   }
   const deleteBtn = (id:number) => {
-    setItems((items) => items.filter((item) => item.id !== id));
+    setItems((item) => item.filter((item) => item.id !== id) );
   }
   const editBtn = (item:IType) => {
     setInputValue(item.text);
     setEditingId(item.id);
   }
-  const toggleInput = (id:number) => {
-    setItems((prev) => prev.map((item) => item.id === id ? {...item, completed:!item.completed} : item));
-  }
   return(
     <>
-      <input value={inputValue} onChange={inputChange}/>
+      <input value={inputValue}/>
       <button onClick={addBtn}>Add</button>
       {items.map((item) => 
-      <li key={item.id}>
-        <input type="checkbox" onChange={() => toggleInput(item.id)} checked={item.completed}/>
-        {item.text}
-        <button onClick={() => deleteBtn(item.id)}>delete</button>
-        <button onClick={() => editBtn(item)}>edit</button>
-      </li>
+        <li key={item.id}>
+          {item.text}
+          <button onClick={()=>deleteBtn(item.id)}>delete</button>
+          <button onClick={()=>editBtn(item)}>edit</button>
+        </li>
       )}
     </>
   );
