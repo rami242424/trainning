f97bf3c768efe72593a4abaae9d2ba4d1af81add
Rami <138556019+rami242424@users.noreply.github.com>
2026-03-26 17:56:20 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,19 @@ interface IType{
 function App(){
   const [inputValue, setInputValue] = useState("");
   const [items, setItems] = useState<IType[]>([]);
+  const [editingId , setEditingId] = useState<number|null>(null);
   const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   }
   const addBtn = () => {
     if(!inputValue.trim()) return;
-    setItems((prev) => [...prev, {id:Date.now(), text:inputValue, completed:false}]);
+    // 수정중
+    if(editingId !==null){
+      // setItems((prev) => prev.map((item) => editingId === item.id ? [...item, {text:inputValue}]: item));
+      setItems((item) => item.map((item) => item.id === editingId ? {...item, text:inputValue} : item));
+    } else {
+      setItems((prev) => [...prev, {id:Date.now(), text:inputValue, completed:false}]);
+    }
     setInputValue("");
   }
   const deleteBtn = (id:number) => {
@@ -21,18 +28,22 @@ function App(){
   }
   const editBtn = (item:IType) => {
     setInputValue(item.text);
-    
+    setEditingId(item.id);
+  }
+  const toggleInput = (id:number) => {
+    setItems((prev) => prev.map((item) => item.id === id ? {...item, completed:!item.completed} : item));
   }
   return(
     <>
       <input value={inputValue} onChange={inputChange}/>
       <button onClick={addBtn}>Add</button>
       {items.map((item) => 
-        <li key={item.id}>
-          {item.text}
-          <button onClick={() => deleteBtn(item.id)}>delete</button>
-          <button onClick={() => editBtn(item)}>edit</button>
-        </li>
+      <li key={item.id}>
+        <input type="checkbox" onChange={() => toggleInput(item.id)} checked={item.completed}/>
+        {item.text}
+        <button onClick={() => deleteBtn(item.id)}>delete</button>
+        <button onClick={() => editBtn(item)}>edit</button>
+      </li>
       )}
     </>
   );
