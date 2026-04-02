5717853ca19841cbda2550909e26f1f6097da87f
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-02 21:12:35 +0900

    commit

--- src/TodoProvider.tsx
+++ src/TodoProvider.tsx
@@ -7,6 +7,87 @@ export interface IType {
     completed: boolean;
 }
 
+// ------------------------state타입
+type State = {
+    items: IType[];
+    inputValue: string;
+    editingId: number | null;
+}
+
+// ------------------------action타입
+type Action = 
+    | { type: "SET_INPUT"; payload: string }
+    | { type: "ADD" }
+    | { type: "START_EDIT"; payload: {id: number, text: string} }
+    | { type: "EDIT"}
+    | { type: "DELETE"; payload: number }
+    | { type: "TOGGLE"; payload: {id: number, checked: boolean}}
+
+// -------------------초기상태
+// -------------------UI 상태를 유지해야 하는 값
+const initialState: State = {
+    items: [],
+    inputValue: "",
+    editingId: null
+};
+
+//------------------------------reducer
+function todoReducer(state:State, action:Action): State {
+    switch(action.type){
+        case "SET_INPUT":
+            return {...state, inputValue: action.payload};
+        case "ADD":
+            if(!state.inputValue.trim()) return state;
+            return {
+                ...state, items: [
+                    ...state.items, {id:Date.now(), text:state.inputValue, completed:false}
+                ],
+                //setItems((prev) => [...prev, {id:Date.now(), text:inputValue, completed:false}]);
+                inputValue: "",
+                editingId: null
+            };
+        case "START_EDIT" :
+            return {
+                ...state,
+                inputValue: action.payload.text,
+                editingId: action.payload.id
+            };
+        case "EDIT" :
+            return {
+                ...state,
+                items : state.items.map((item) => 
+                    item.id === state.editingId 
+                    ? {...item, text: state.inputValue}
+                    : item
+                ),
+                inputValue: "",
+                editingId: null
+            }
+        case "DELETE":
+            return {
+                ...state,
+                items: state.items.filter((item) => 
+                    item.id !== action.payload
+                )
+            };
+        case "TOGGLE":
+            return {
+                // setitem((prev) => prev.map((item) => item.id === id ? ...item, {completed: checked}: item))
+                ...state,
+                items : state.items.map((item) => 
+                    action.payload.id === item.id
+                    ? {...item, completed: action.payload.checked}
+                    : item
+                )
+            }
+    }
+    return(
+
+    );
+}
+
+
+
 function useTodo(){
     const context = useContext(TodoContext);
     if(!context){
